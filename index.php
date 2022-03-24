e23c5b92d6752aeda32ff7417201894cab3e97b3
giovane <giovane_05silva@hotmail.com>
2022-03-24 10:33:35 -0300

    Refatoração

--- index.php
+++ index.php
@@ -23,74 +23,64 @@
     </div>
 
     <div class="meio">
-            <h1 id="titulo">Bem-Vindo a sua comunidade científica!</h1>
-            <!-- p id=subtitulo>Complete com suas informações:</p -->
-    <form>      
-        <fieldset id="campo-email" class="group">
-            <div class="campo">
+            <h1 id="tituloCadastro">Bem-Vindo a sua comunidade científica!</h1>
+            <h3>Complete com as suas informações </h3>
+        <form>      
+            <fieldset id="login">
                 <label for="email"><strong>Email:</strong></label>
                 <input type="email" name="email" id="email" required placeholder="Ex.: giovane@hotmail.com">
-            </div> 
-            <div class="campo">
+                
                 <label for="senha"><strong>Senha:</strong></label>
                 <input type="password" name="senha" id="senha" required placeholder="**********">
-            </div>
-        </fieldset>
-            <ul type="none"><li><a href="forget">Esqueceu a sua senha?</a></li></ul>
-            <button type="submit" class="btn">Log In</button>
-    </form>
-        <button type="submit" class="btn2">Cadastrar</button>
+                
+                <button type="submit" id="botaoLogin">Log In</button>
+
+                <a href="#">Esqueceu a sua senha?</a>
+                <button type="submit" id="botaoCadastrar">Cadastrar</button>
+            </fieldset>
+        </form>
         <!-- img src="/wordpress/wp-content/themes/scientific/logo_sem_fundo2_aumentada.png" alt="Logo Ciência" -->
     </div>
 
-    <!-- div class="projeto" -->
+    <div class="inferior">
         <section id="projeto"><h2>Projeto Integrador</h2>
             <p>Este projeto tem como objetivo primário a criação de uma plataforma rede social na qual cientistas 
             de todo o mundo sejam capazes de compartilhar informações uns com os outros, dessa forma aumentando a taxa de
             desenvolvimento científico de todo o mundo</p>
         </section>
-    </div>
 
-    <!-- div class="missao" -->
         <section id="missao"><h3>Missão</h3>
             <p>Por meio de um trabalho desenvolvido com ética, comprometimento e eficiência, temos a missão de 
             transformar a realidade de empresas, negócios e pessoas, aumentando os seus resultados no universo web e mobile de 
             maneira contínua.</p>
         </section>
-    <!-- /div --> 
 
-    <!-- div class="visao" -->
         <section id="visao"><h3>Visão</h3>
             <p>Ser uma empresa de desenvolvimento de aplicativos excelente em inovação e criatividade que é parceira 
             dos nossos clientes, promove grandes resultados e segue seus valores para consolidar-se no mercado como referência no 
             desenvolvimento de aplicativos e em serviços e soluções web e mobile.</p>
         </section>
-    <!-- /div --> 
-    
-    <!-- div class="valores" -->
-        <nav><h3>Valores</h3></nav>
-        <section id="valores">
-            <div class="texto">
-                Inovar na proposta
-                Revolucionar na ação
-                Surpreender a cada contato
-                Entregar sempre o melhor
-                Ser honesto, profissional e responsável
-                Crescer e aprender todos os dias
-                Viver e trabalhar com alegria
-                Respirar tecnologia 24 horas
-            </div>
+
+        <section id="valores"> <h3>Valores</h3>
+                <ul>
+                    <li>Inovar na proposta</li>
+                    <li>Revolucionar na ação</li>
+                    <li>Surpreender a cada contato</li>
+                    <li>Entregar sempre o melhor</li>
+                    <li>Ser honesto, profissional e responsável</li>
+                    <li>Crescer e aprender todos os dias</li>
+                    <li>Viver e trabalhar com alegria</li>
+                    <li>Respirar tecnologia 24 horas</li>
+                </ul>
         </section>
-    <!-- /div -->
-    
-    <!-- div class="quem" -->
-        <h3>Quem somos?</h3>
-        <section id="quem">Somos uma empresa de desenvolvimento de aplicativos para quem 
+
+        <section id="quem"><h3>Quem somos?</h3>
+            Somos uma empresa de desenvolvimento de aplicativos para quem 
             gosta de inovar, superar expectativas e sair da caixa. Sonhamos alto e junto com 
             o nosso time alcançamos excelentes resultados, contribuindo para o sucesso de 
             empresas, negócios e pessoas. 
         </section>
-    <!-- /div -->
+    </div>
     <script type="text/javascript" src="scripts.js"></script>
 </body>
 </html>
