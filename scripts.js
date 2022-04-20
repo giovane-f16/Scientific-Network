fa351349e7e44a90a769640d2c85076675d222c8
giovane <giovane_05silva@hotmail.com>
2022-04-20 12:26:40 -0300

    JavaScript seção Cadastro

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
+/*
 document.getElementById("botaoDadosCadastrados").addEventListener("click", function(){
     alert("clicou");
     getElementById("nome").value='';
     getElementById("emailCadastro").value='';
     getElementById("senhaCadastro").value='';
     //location.reload();
-});
+}); */
 
 document.getElementById("botaoCadastrar").addEventListener("click", function(){
-    alert("deu certo");
+    document.querySelector(".cadastro").style.display = "block";
 })
